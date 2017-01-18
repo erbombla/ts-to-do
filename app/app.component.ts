710e12b71568768d12702a1f7ff142a10d88d635
Eric Bomblatus <ebomblatus@gmail.com>
2017-01-18 11:21:46 -0800

    add content to app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h1>To Do List <small>{{month}}/{{day}}/{{year}}</small></h1>
+        <h3 class="bg-info">Current Task: {{currentFocus}}</h3>
+        <ul>
+          <li>{{firstTask.description}}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
   `
 })
 
 export class AppComponent {
-
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask = {
+    description: "Finish Angular homework for Epicodus"
+  }
 }
